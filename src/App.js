7d301f982b6cd26f3635bf5b8b905b9c2875fe1f
GediminasKul <gediminas.kulakauskas@gmail.com>
2022-07-04 11:37:53 +0300

    Added paths, pages, started header component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router';
+
+import Addpage from './pages/add/addpage';
+import Homepage from './pages/home/homepage';
+import Loginpage from './pages/login/loginpage';
+import Registerpage from './pages/register/registerpage';
+import Header from './components/header/header';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path={'/'}>
+          <Homepage />
+        </Route>
+        <Route path={'/add'}>
+          <Addpage />
+        </Route>
+        <Route path={'/login'}>
+          <Loginpage />
+        </Route>
+        <Route path={'/register'}>
+          <Registerpage />
+        </Route>
+        <Route path="*">
+          <h2>Page Not found</h2>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,3 @@
+import { useContext } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { AuthContext } from '../store/AuthContext';
